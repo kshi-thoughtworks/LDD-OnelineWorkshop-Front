d61c8a1c1eee395bae06aeb6f11217b61a5554d7
JianChen <jianchen@thoughtworks.com>
2020-06-08 16:00:58 +0800

    [chenjian] draw register page

--- src/components/Register.vue
+++ src/components/Register.vue
@@ -1,5 +1,27 @@
 <template lang="pug">
-  <p>Hi Register</p>
+  div.register-container
+    h1 欢迎使用精益数据工作坊
+    h2 Lean Data Discovery
+    div.form-container
+        h3 注册新账号
+        div.line
+        form
+            label 昵称
+            input(type='text').input-box
+            label 邮箱
+            input(type='email').input-box
+            label 密码
+            input(type='password').input-box
+            p.agreement
+                input(type='checkbox')
+                span 我已阅读并同意
+                a 《LDD网络服务使用协议》
+                span 和
+                a 《LDD用户隐私条款》
+            input(type='submit' value='注册')
+            p.to-login
+                span 已有账号？请
+                a.login 登录
 </template>
 
 <script>
@@ -9,19 +31,91 @@
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-    h3 {
-        margin: 40px 0 0;
+<style scoped lang="scss">
+    .register-container {
+        background-color: #37115a;
+        background-image: url('../assets/lego-background.svg');
+        background-repeat: no-repeat;
+        background-size: contain;
+        box-sizing: border-box;
+        min-height: 100%;
+        width: 100%;
+        padding-top: 50px;
     }
-    ul {
-        list-style-type: none;
-        padding: 0;
+    h1, h2 {
+        color: #ffffff;
     }
-    li {
-        display: inline-block;
-        margin: 0 10px;
+    .line {
+        width: 560px;
+        height: 1px;
+        background-color: #dedede;
     }
-    a {
-        color: #42b983;
+    .form-container {
+        width: 620px;
+        height: 689px;
+        border-radius: 8px;
+        box-shadow: 0 3px 20px 0 rgba(40, 29, 50, 0.4);
+        background-color: #ffffff;
+        margin: auto;
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+
+        h3 {
+            font-size: 24px;
+            font-weight: 600;
+            color: #6c0dbc;
+        }
+        form {
+            width: 560px;
+            label {
+                display: block;
+                text-align: left;
+                font-size: 14px;
+                font-weight: 600;
+                margin-top: 30px;
+                margin-bottom: 10px;
+            }
+            .input-box {
+                width: 100%;
+                height: 60px;
+                border-radius: 4px;
+                background-color: #f5f3f6;
+                border: none;
+            }
+            .agreement {
+                text-align: left;
+                margin: 35px 0;
+                font-size: 14px;
+                span {
+                    font-weight: 600;
+                    color: #000000;
+                }
+                a {
+                    color: #6d6e71;
+                }
+            }
+            input[type=submit] {
+                width: 100%;
+                height: 50px;
+                border-radius: 4px;
+                border: none;
+                background-color: var(--violet-blue);
+                background: #6c0dbc;
+                color: #ffffff;
+                font-size: 16px;
+                font-weight: 600;
+                letter-spacing: 10px;
+            }
+            .to-login {
+                text-align: center;
+                font-size: 14px;
+                color: #6d6e71;
+                .login {
+                    font-weight: 600;
+                    color: #6c0dbc;
+                }
+            }
+        }
     }
 </style>
