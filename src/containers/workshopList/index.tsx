68c702d1ee73c07d5b15c851527529f6301916ef
Wanlu Niu <niu.wanlu@rea-group.com>
2020-06-17 14:20:35 +0800

    [wanlu] add style to workshop list page

--- src/containers/workshopList/index.tsx
+++ src/containers/workshopList/index.tsx
@@ -1,9 +1,14 @@
 import Vue from 'vue'
 import { Component } from 'vue-property-decorator'
 import axios from 'axios'
+import {Avatar} from 'ant-design-vue';
 import './index.scss'
 
-@Component
+@Component({
+    components: {
+        'a-avatar': Avatar
+    }
+})
 export default class WorkshopList extends Vue{
 
     workshop_list = [
@@ -25,16 +30,27 @@ export default class WorkshopList extends Vue{
 
     render(h){
         return (
-          <div class="bench">
-            {this.workshop_list.map(value => 
-                <div class="bench-card">
-                    <div class="bench-card-bg">
-                        <p>{value.description}</p>
+          <div class="workshop">
+            <header class="bench-header">
+                <router-link to="/" class="logo"/>
+                <span class="bench-header-title">我的工作台</span>
+                <a-avatar style="color: #f56a00; backgroundColor: #fde3cf">
+                    U
+                </a-avatar>
+            </header>
+            <div class="bench">
+                {this.workshop_list.map(value => 
+                    <div class="bench-card">
+                        <div class="bench-card-bg">
+                            <div class="bench-card-cover">
+                                <p>{value.description}</p>
+                            </div>
+                        </div>
+                        <p class="bench-card-name">{value.name}</p>
+                        <p class="bench-card-date">{value.created_at}</p>
                     </div>
-                    <p class="bench-card-name">{value.name}</p>
-                    <p class="bench-card-date">{value.created_at}</p>
-                </div>
-            )}
+                )}
+            </div>
           </div>
         )
       }
